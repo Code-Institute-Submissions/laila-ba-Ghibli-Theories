26f5bea0e81ad861861607c82ed3508f45b27dde
laila-ba <75024926+laila-ba@users.noreply.github.com>
2021-08-17 19:51:43 +0100

    Add files via upload
    added lists and curly bracket

--- app.py
+++ app.py
@@ -88,9 +88,11 @@ def profile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-
+    posts = list(mongo.db.posts.find({"posted_by": session["user"]}))
     if session["user"]:
-        return render_template("profile.html", username=username)
+        return render_template("profile.html",posts=posts, username=username)
+
+
 
     return redirect(url_for("login"))
 
